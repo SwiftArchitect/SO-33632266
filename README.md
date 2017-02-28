c7f5784b4c233eadc25d5a02981e635e896e988b
Xavier Schott <assembla@thegothicparty.com>
2017-02-28 11:14:22 -0800

    SO-33632266

---
+++ README.md
@@ -0,0 +1,20 @@
+# Animate Text Change of UILabel: Scroll Animation on iOS
+
+---
+
+### Stack Overflow Question 33632266
+
+Xcode project & source code related to [Use a UIView Extension in Swift](http://stackoverflow.com/a/33705634/218152) anwser.
+
+---
+
+**Instructions:**
+
+1. build & run
+2. tap Animate
+
+**References:**
+
+- [SO-33632266](http://stackoverflow.com/questions/33632266/animate-text-change-of-uilabel) on Stack Overflow
+- This and other answers posted on [Swift Recipes](http://swiftarchitect.com/recipes/)
+
---
+++ SO-33632266/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  SO-33632266
+//
+//  Copyright © 2017 Xavier Schott
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import UIKit
+
+// Usage: insert view.pushTransition right before changing content
+extension UIView {
+    func pushTransition(_ duration:CFTimeInterval) {
+        let animation:CATransition = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name:
+            kCAMediaTimingFunctionEaseInEaseOut)
+        animation.type = kCATransitionPush
+        animation.subtype = kCATransitionFromTop
+        animation.duration = duration
+        layer.add(animation, forKey: kCATransitionPush)
+    }
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    var count:Int = 0
+    
+    @IBAction func doAction(_ sender: AnyObject) {
+        if let aLabel = label {
+            aLabel.pushTransition(0.4)
+            aLabel.text = "\(count)"
+            count += 1
+        }
+    }
+}
+
